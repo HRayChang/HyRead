90002d37e9bc8526464d0b3b6898cfb206116857
RayChang <raychang861205@gmail.com>
2024-03-09 13:55:52 +0800

    set collection view on MainVC

--- HyRead/Controllers/MainViewController/MainViewController.swift
+++ HyRead/Controllers/MainViewController/MainViewController.swift
@@ -8,22 +8,14 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    // IBoutlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "我的書櫃"
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
