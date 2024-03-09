0d6ad2b0862959cb86d9ab869e2b544901db0a38
RayChang <raychang861205@gmail.com>
2024-03-09 23:58:54 +0800

    add ImageView and Label in ContainerView

--- HyRead/Views/BookCollectionViewCell/BookCollectionViewCell.swift
+++ HyRead/Views/BookCollectionViewCell/BookCollectionViewCell.swift
@@ -11,6 +11,8 @@ class BookCollectionViewCell: UICollectionViewCell {
 
     // IBoutlets
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var bookImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
