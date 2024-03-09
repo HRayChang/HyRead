057b7bc2b044eeb1ee8385e4c6cc452993c20319
RayChang <raychang861205@gmail.com>
2024-03-09 23:49:09 +0800

    create container view in content view in BookCollectionViewCell by xib

---
+++ HyRead/Views/BookCollectionViewCell/BookCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  BookCollectionViewCell.swift
+//  HyRead
+//
+//  Created by Ray Chang on 2024/3/9.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+
+    // IBoutlets
+    @IBOutlet weak var containerView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
