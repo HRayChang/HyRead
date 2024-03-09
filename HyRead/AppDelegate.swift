49f715dfc130a1462a65646efb6ab1d85d262816
RayChang <raychang861205@gmail.com>
2024-03-09 13:34:15 +0800

    add navigationController and set mainVC title

--- HyRead/AppDelegate.swift
+++ HyRead/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     
         return true
